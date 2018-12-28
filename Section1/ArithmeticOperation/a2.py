9f698cdf8666e348889a4cbd7c9f5422d2511fc9
Vidit Shah <vidit0210@gmail.com>
2018-12-28 21:02:48 +0530

    Loops

---
+++ Section1/ArithmeticOperation/a2.py
@@ -0,0 +1,12 @@
+# Arithmetic operation
+# There is Nothing like Constants in Python
+import math
+PI = 3.14
+print(round(PI))
+print(abs(-3))
+
+# Type Coversion
+x = input("Enter the Value:")
+print(int(x))
+print(float(x))
+print(bool(x))
---
+++ Section1/Loops/if.py
@@ -0,0 +1,5 @@
+age = 22
+if age >= 22:
+    print("Adult")
+else:
+    print("Not an Adult")
