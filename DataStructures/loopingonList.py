021659a493d4226af938daef877d751d95c9be64
Vidit Shah <vidit0210@gmail.com>
2018-12-30 23:13:07 +0530

    listv2

---
+++ DataStructures/loopingonList.py
@@ -0,0 +1,6 @@
+l = ['a', 'b', 'c']
+for index, letter in enumerate(l):
+    print(index, letter)
+
+for letter in l:
+    print(letter)
---
+++ DataStructures/packing.py
@@ -0,0 +1,8 @@
+l = [3, 4, 5, 6, 7, 7, 7, 7, 7, 9]
+first, second, *others = l
+print(first)
+print(second)
+print(others)
+details = ['Vidit', 'O+', '9769748749', 'India', 'Mumbai', '400004']
+name, blood_group, *extra_detail, pin = details
+print(extra_detail)
